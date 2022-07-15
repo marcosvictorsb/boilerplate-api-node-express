387b4204065afc979dd601bb094d6fa6df9185c8
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-15 18:50:20 -0300

    feat: create httpStatusCode

---
+++ src/protocols/https.js
@@ -0,0 +1,49 @@
+const HttpStatusCode = {
+  OK: 200,
+  Created: 201,
+  noContent: 204,
+  notFound: 404,
+  Conflict: 409,
+  serverError: 500
+}
+
+const OK = (response) => ({
+  status: HttpStatusCode.OK,
+  body: { result: response }
+})
+
+const conflict = (error) => ({
+  status: HttpStatusCode.Conflict,
+  body: { error }
+})
+
+const noContent = (message) => ({
+  status: HttpStatusCode.noContent,
+  body: { result: message }
+})
+
+const created = (response) => ({
+  status: HttpStatusCode.Created,
+  body: { result: response }
+})
+
+const notFound = (response) => ({
+  status: HttpStatusCode.notFound,
+  body: { result: response }
+})
+
+const serverError = (error) => ({
+  status: HttpStatusCode.serverError,
+  body:{ error }
+})
+
+module.exports = {
+  HttpStatusCode,
+  created,
+  conflict,
+  serverError,
+  notFound,
+  OK,
+  noContent
+}
+Footer
