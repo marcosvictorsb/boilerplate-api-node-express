ddfa9974c20e5a9ff64c707fc6bab912d3983805
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-16 17:23:15 -0300

    chore: remove comment

--- src/domains/customers/routes/users-routes.js
+++ src/domains/customers/routes/users-routes.js
@@ -1,7 +1,5 @@
-const Router = require('express')
 const { getController } = require('../factories')
 
-// const routes = Router()
 const controller = getController();
 
 exports.loadRoutes = loadRoutes = (server) => {
