3a6a6f56b7bf3115d076775062981623593fc422
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-16 16:06:05 -0300

    feat: config bodyparser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const routers = require('./src/config/routers');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 routers(app);
 
