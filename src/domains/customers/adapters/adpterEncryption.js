1affc8997fe4ef02638496390c3b9b0e544fb156
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-16 17:58:15 -0300

    feat: create adapter to encryption password

---
+++ src/domains/customers/adapters/adpterEncryption.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs')
+
+
+class AdpaterEncryption {
+  static generateHashPassword(password) {
+    const SALT_ROUNDS = 10;
+    const salt = bcrypt.genSaltSync(SALT_ROUNDS)
+    const hash = bcrypt.hashSync(password, salt)
+
+    return hash;
+  }
+}
+
+module.exports = AdpaterEncryption
