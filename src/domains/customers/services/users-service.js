a004bf2a068c4a3afabc59b565518292de52b603
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-16 17:59:04 -0300

    chore: encryption password to create user

--- src/domains/customers/services/users-service.js
+++ src/domains/customers/services/users-service.js
@@ -1,4 +1,5 @@
 const UserRepository = require('../repositories/users-repository')
+const AdpaterEncryption = require('../adapters/adpterEncryption');
 const enumHelperUser = require('../../../helpers/enumHelperUser')
 const logger = require('../../../config/logger');
 const { created, conflict, serverError } = require('../../../protocols/https')
@@ -12,15 +13,16 @@ class UserService {
 
   create(user){
     try {
-      const { name, email, password, repeatPassword } = user;
+      const { name, email, password } = user;
 
       const userAlreadyExists = this.repository.getByEmail(email);
       if(!!userAlreadyExists) {
         this.logger.info(`[CREATE USER SERVICE] - ${this.enumHelperUser.user.alreadyExists} : ${email}`)
         return conflict(this.enumHelperUser.user.alreadyExists);    
       }      
-        
-      const newUser = { name, email, password, repeatPassword }
+      
+      
+      const newUser = { name, email, passwordEncryption: AdpaterEncryption.generateHashPassword(password) }
       const result = this.repository.create(newUser);    
       if(!result) {
         this.logger.info(`[CREATE USER SERVICE] - ${this.enumHelperUser.user.errorToCreateUser}`)
