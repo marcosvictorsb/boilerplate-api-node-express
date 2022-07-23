e70048e820fc661f787f750dbea1b9cb0d564fb8
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-23 19:41:28 -0300

    refactor: rename userService to CustomerService

--- src/domains/authentication/service/authentication-service.js
+++ src/domains/authentication/service/authentication-service.js
@@ -1,19 +1,19 @@
 const { conflict, OK, serverError } = require('../../../protocols/https');
 const enumHelperUser = require('../../../helpers/enumHelperUser');
-const UserService = require('../../customers/services/users-service');
+const CustomerService = require('../../customers/services/customer-service');
 const logger = require('../../../config/logger');
 const AdapterToken = require('../adapter/adapterToken');
 
 class AuthenticationService {
   constructor(params = {}) {
     this.enumHelperUser = params.enumHelperUser || enumHelperUser;
-    this.userService = params.userService || new UserService();
+    this.customerService = params.customerService || new CustomerService();
     this.logger = params.logger || logger;
   }
 
   async authenticate(email, password) {
     try {
-      const result = await this.userService.getByEmail(email);
+      const result = await this.customerService.getByEmail(email);
 
       const { result: user } = result.body;
       if (!user) {
@@ -21,7 +21,8 @@ class AuthenticationService {
         return conflict(this.enumHelperUser.user.notFoundUser);
       }
 
-      const isCompare = await this.userService.comparePasswords(password, user.passwordEncryption);
+      const isCompare = await this.customerService
+        .comparePasswords(password, user.passwordEncryption);
       if (!isCompare) {
         this.logger.info('the password is incorrect');
         return conflict(this.enumHelperUser.user.emailOrPassword);
