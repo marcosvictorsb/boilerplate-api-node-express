7af2d7ebd6933f5d551928f5155fd5c09a9550eb
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-30 20:04:57 -0300

    feat: install helmet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const routers = require('./src/config/routers');
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet);
 
 routers(app);
 
