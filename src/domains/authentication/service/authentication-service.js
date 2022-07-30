1781c328e8ffc367c50f854a2fc50273c2c13a79
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-07-30 18:25:10 -0300

    refactor: change user to customer

--- src/domains/authentication/service/authentication-service.js
+++ src/domains/authentication/service/authentication-service.js
@@ -16,22 +16,22 @@ class AuthenticationService {
     try {
       const result = await this.customerService.getByEmail(email);
 
-      const { result: user } = result.body;
-      if (!user) {
+      const { result: customer } = result.body;
+      if (!customer) {
         this.logger.info(`User not found with email ${email}`);
         return conflict(this.enumHelperCustomer.customer.notFoundUser);
       }
 
       const isCompare = await this.customerService
-        .comparePasswords(password, user.passwordEncryption);
+        .comparePasswords(password, customer.password);
       if (!isCompare) {
         this.logger.info('the password is incorrect');
         return conflict(this.enumHelperCustomer.customer.emailOrPassword);
       }
       /* eslint-disable no-param-reassign */
-      user.passwordEncryption = undefined;
-      user.token = this.adapterToken.sign(user.id);
-      return OK(user);
+      customer.password = undefined;
+      customer.token = this.adapterToken.sign(customer.id);
+      return OK(customer);
     } catch (error) {
       this.logger.error('[AuthenticationService] - error to compare the password');
       return serverError(error.message);
