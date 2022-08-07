3433a23912c399e75541a1d5ac7d159befab6a18
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-08-07 13:54:32 -0300

    test: refactor unit test in the method created

--- test/domains/customers/services/users-service.test.js
+++ test/domains/customers/services/users-service.test.js
@@ -6,6 +6,7 @@ const CustomerRepository = require('../../../../src/domains/customers/repositori
 const CustomerService = require('../../../../src/domains/customers/services/customer-service');
 const enumHelperCustomer = require('../../../../src/helpers/enumHelperCustomer');
 const { HttpStatusCode } = require('../../../../src/protocols/https');
+const AdapterEncryption = require('../../../../src/domains/customers/adapter/adapterEncryption')
 
 describe('CUSTOMER SERVICE', () => {
   beforeEach(() => {
@@ -16,16 +17,21 @@ describe('CUSTOMER SERVICE', () => {
       repository: this.repository,
       enumHelperCustomer,
       logger: this.logger,
+      adapterEncryption: AdapterEncryption,
     });
 
-    this.user = {
+    this.service.adapterEncryption = {
+      generateHashPassword: stub().resolves('encryption_password'),
+    }
+
+    this.customer = {
       name: 'any_name',
       email: 'any_email',
       password: 'any_password',
     };
 
     this.service.repository = {
-      getByEmail: stub().resolves(undefined),
+      getByEmail: stub().resolves([]),
       create: stub().resolves(undefined),
     };
 
@@ -35,54 +41,60 @@ describe('CUSTOMER SERVICE', () => {
   });
 
   describe('CREATE', () => {
-    it('return status code 409 when exist user with same email', async () => {
+    it('return status code 409 when exist customer with same email', async () => {
       this.service.repository.getByEmail = stub().resolves([{ name: 'any_name' }]);
 
-      const result = await this.service.create(this.user);
+      const result = await this.service.create(this.customer);
 
       assert.calledOnce(this.service.repository.getByEmail);
-      assert.calledWith(this.service.repository.getByEmail, this.user.email);
+      assert.calledWith(this.service.repository.getByEmail, this.customer.email);
       assert.calledOnce(this.service.logger.info);
       expect(result.status).to.eq(HttpStatusCode.Conflict);
     });
 
     it('return status code 409 when repository create result rejected', async () => {
-      const result = await this.service.create(this.user);
+      const result = await this.service.create(this.customer);
 
       assert.calledOnce(this.service.repository.getByEmail);
-      assert.calledWith(this.service.repository.getByEmail, this.user.email);
+      assert.calledWith(this.service.repository.getByEmail, this.customer.email);
+      assert.calledOnce(this.service.adapterEncryption.generateHashPassword)
+      assert.calledWith(this.service.adapterEncryption.generateHashPassword,
+        this.customer.password)
       assert.calledOnce(this.service.repository.create);
       expect(result.status).to.eq(HttpStatusCode.Conflict);
     });
 
-    it('return status code 201 when create new user', async () => {
-      this.service.repository.create = stub().resolves([]);
+    it('return status code 201 when create new customer', async () => {
+      this.customer.password = 'encryption_password'
 
-      const result = await this.service.create(this.user);
+      await this.service.create(this.customer);
 
       assert.calledOnce(this.service.repository.getByEmail);
-      assert.calledWith(this.service.repository.getByEmail, this.user.email);
+      assert.calledWith(this.service.repository.getByEmail, this.customer.email);
+      assert.calledOnce(this.service.adapterEncryption.generateHashPassword);
+      assert.calledWith(this.service.adapterEncryption.generateHashPassword,
+        this.customer.password);
       assert.calledOnce(this.service.repository.create);
-      expect(result.status).to.eq(HttpStatusCode.Created);
+      assert.calledWith(this.service.repository.create, this.customer);
     });
 
-    it('return status code 500 when happen in the moment to get user by email', async () => {
+    it('return status code 500 when happen in the moment to get customer by email', async () => {
       this.service.repository.getByEmail = stub().rejects();
 
-      const result = await this.service.create(this.user);
+      const result = await this.service.create(this.customer);
 
       assert.calledOnce(this.service.repository.getByEmail);
-      assert.calledWith(this.service.repository.getByEmail, this.user.email);
+      assert.calledWith(this.service.repository.getByEmail, this.customer.email);
       expect(result.status).to.eq(HttpStatusCode.serverError);
     });
 
-    it('return status code 500 when happen in the moment to create user', async () => {
+    it('return status code 500 when happen in the moment to create customer', async () => {
       this.service.repository.create = stub().rejects();
 
-      const result = await this.service.create(this.user);
+      const result = await this.service.create(this.customer);
 
       assert.calledOnce(this.service.repository.getByEmail);
-      assert.calledWith(this.service.repository.getByEmail, this.user.email);
+      assert.calledWith(this.service.repository.getByEmail, this.customer.email);
       expect(result.status).to.eq(HttpStatusCode.serverError);
     });
   });
