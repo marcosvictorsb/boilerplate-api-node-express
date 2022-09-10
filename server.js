ec4bd65750f456e86cd27187b6f91e9b1d8ee9f9
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-09-10 00:16:36 -0300

    a

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
+const os = require('os');
 const logger = require('./src/config/logger');
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
+
+const currentDateTime = new Date().toLocaleString();
+const networkInterfaces = os.networkInterfaces();
+
+const response = {
+  currentDateTime,
+  message: 'server online',
+  networkInterfaces,
+};
+
+logger.info(response);
 
 app.listen(PORT, () => logger.info(`SERVER RUNNING IN ${PORT}`));
--- src/domains/health/services/health-service.js
+++ src/domains/health/services/health-service.js
@@ -1,3 +1,5 @@
+const os = require('os');
+
 class HealthService {
   constructor(params = {}) {
     this.logger = params.logger;
@@ -7,9 +9,12 @@ class HealthService {
   async health() {
     try {
       const currentDateTime = new Date().toLocaleString();
+      const networkInterfaces = os.networkInterfaces();
+
       const response = {
         currentDateTime,
         message: 'server online',
+        networkInterfaces,
       };
       return this.httpResponseStatusCode.OK(response);
     } catch (error) {
